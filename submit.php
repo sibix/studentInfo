763bb9e7d343f145a55e9fad72054be2cf2cfad1
sibi xavier <sibixavier@live.com>
2014-12-03 23:23:16 +0530

    Create submit.php

---
+++ submit.php
@@ -0,0 +1,11 @@
+
+<h1>Please fill in the details</h1>
+
+
+	<form action="testo.php" method="post">
+
+		Student name<input type="text" name="name"><br>
+		Parent name <input type="text" name="parentName"><br>
+		Mobile number<input type="text" name="mobileNumber"><br>
+		<input type="submit">
+	</form>	
