c9d0a637f2856bdbb491d3e85431806f2523960c
sibi xavier <sibixavier@live.com>
2014-12-08 09:42:45 +0530

    Update update.php

--- update.php
+++ update.php
@@ -12,30 +12,99 @@ $servername = "localhost";
 			}
 			echo "Connected successfully<br>";
 
-
-
-
 $Newname ='arjun';
 $sql= "SELECT * FROM `studentdata` where name like '$Newname'";
 $result = mysqli_query($conn,$sql);
 
-echo "<table>";
-while ($row=mysqli_fetch_array($result))
-{
-echo "<tr>";
-	echo "<td>".$row["name"]."</td>";
-	echo "<td>".$row["parentname"]."</td>";
-	echo "<td>".$row["email"]."</td>";
-	
-echo "</tr>";
+
+while ($row=mysqli_fetch_array($result)) {
+	$name1=$row["name"];
+	$parentname1=$row["parentname"];
+	$email1=$row["email"];
 }
-echo "</table>"
+
+
+echo "<table>";
+	echo "<tr>";
+
+		echo"<td>".$name1."</td>";
+		echo"<td>".$parentname1."</td>";
+		echo"<td>".$email1."</td>";
+
+	echo "</tr>";
+echo "</table>";
+
+
+
+
+
 
 ?>
-<form action="testo.php" method="post">
+<form action="update.php" method="post">
 
-		Student name<input type="text" name="name"><br>
-		Parent name <input type="text" name="parentName"><br>
-		Mobile number<input type="text" name="mobileNumber"><br>
+		New Student name<input type="text" name="name">
+		New Parent name <input type="text" name="parentName">
+		New Mobile number<input type="text" name="mobileNumber">
 		<input type="submit">
 	</form>	
+
+	<?php
+		if(isset($_POST['name'])){ $name2 = $_POST['name'];
+			$sql = "UPDATE studentdata SET name='$name2'WHERE name='$name1'";
+
+
+			if (mysqli_query($conn, $sql)) {
+    echo "New name saved sccessfully<br>";
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+
+
+
+ } 
+
+
+
+
+
+		if(isset($_POST['parentName'])){ $parentname2 = $_POST['parentName']; 
+			$sql = "UPDATE studentdata SET parentname='$parentname2'WHERE name='$name1'";
+
+
+
+
+
+		if (mysqli_query($conn, $sql)) {
+    echo "New parent saved sccessfully<br>";
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+} 
+
+
+
+		if(isset($_POST['mobileNumber'])){ $mobile2 = $_POST['mobileNumber'];
+		$sql = "UPDATE studentdata SET email='$mobile2'WHERE name='$name1'"; 
+
+
+	if (mysqli_query($conn, $sql)) {
+    echo "New mbile saved sccessfully<br>";
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+}
+
+	
+
+
+
+	?>
+
+		
+	
+
+	
+
+
+
